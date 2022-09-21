8c50bde394eed52fcf53ae1b143172b27abdaa24
liyamin <919236846@qq.com>
2022-09-21 10:16:27 +0800

    测试图片能否成功加载

---
+++ src/test/java/ImageTest.java
@@ -0,0 +1,24 @@
+import jdk.nashorn.internal.AssertsEnabled;
+import org.junit.Test;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+/**
+ * @author li yamin
+ * @create 2022-09-21
+ */
+public class ImageTest {
+    @Test
+    public void test() throws IOException {
+        try{
+            // this.getClass()可以换成ImageTest.class
+            BufferedImage image = ImageIO.read(this.getClass().getClassLoader().getResourceAsStream("images/bulletD.gif"));
+            System.out.println(image != null);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+}
